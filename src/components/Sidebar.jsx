aed0b75f850c309c25a42b071a08076ba05546ce
Michter99 <63754580+Michter99@users.noreply.github.com>
2020-07-11 13:17:46 -0500

    Add files via upload

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,31 +1,36 @@
 import React from "react";
 
 function Sidebar(){
-    return <div className="wrapper">
-    <nav id="sidebar">
+  return <div className="wrapper">
+  <nav id="sidebar">
 
-        <ul className="list-unstyled components">
-            <img src="Images/profile.png" alt="profilePic" className="profilePic"></img>
-            <p>Miguel Richter</p>
-                <li>
-                    <a href="#"><i className="fas navIcon fa-th-large"></i>General</a>
-                    <a href="#"><i className="fas navIcon fa-boxes"></i>Recursos Materiales</a>
-                    <a href="#"><i className="fas navIcon fa-wallet"></i>Recursos Financieros</a>
-                    <a href="#"><i className="fas navIcon fa-users-cog"></i>Recursos Humanos</a>
-                    <a href="#"><i className="fas navIcon fa-chalkboard-teacher"></i>Espacios de Trabajo</a>
-                </li>
-            </ul>
+    <button type="button" id="sidebarCollapse" className="btn">
+      <i className="fas fa-bars"></i>
+    </button>
+    <ul className="list-unstyled components">
+      <img src="Images/profile.png" alt="profilePic" className="profilePic"></img>
+      <p>Miguel Richter</p>
+      <li>
+        <a href="#"><i className="fas navIcon fa-th-large"></i>General</a>
+        <a href="#"><i className="fas navIcon fa-boxes"></i>Recursos Materiales</a>
+        <a href="#"><i className="fas navIcon fa-wallet"></i>Recursos Financieros</a>
+        <a href="#"><i className="fas navIcon fa-users-cog"></i>Recursos Humanos</a>
+        <a href="#"><i className="fas navIcon fa-chalkboard-teacher"></i>Espacios de Trabajo</a>
+      </li>
+    </ul>
 
-            <ul className="list-unstyled CTAs">
-                <li>
-                    <img src="Images/logo-white.png" alt="panterasLogo" className="panterasLogo"></img>
-                </li>
-                <a href="#">Aviso legal</a>
-                <a href="#">Políticas</a>
-                <a href="#">Contacto</a>
-            </ul>
+    <div className="CTAs">
+      <ul className="list-unstyled">
+        <li>
+          <img src="Images/logo-white.png" alt="panterasLogo" className="panterasLogo"></img>
+        </li>
+          <a href="#">Aviso legal</a>
+          <a href="#">Políticas</a>
+          <a href="#">Contacto</a>
+      </ul>
+    </div>
 
-    </nav>
+  </nav>
 </div>
 }
 
