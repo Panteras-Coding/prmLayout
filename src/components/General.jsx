793e9402aa703836136496d111f03e643b0ea4b1
araimond99 <alanraimondc@outlook.com>
2020-08-07 19:43:25 -0500

    Settings Tab Alan Raimond

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import TopGen from "./GeneralComponents/TopGen";
 import Interactividad from "./GeneralComponents/Interactividad";
 import Feed from "./GeneralComponents/Feed";
-
+import Settings from "./GeneralComponents/Settings";
 function General(){
   return <div id="content">
     <TopGen />
     <Interactividad />
     <Feed />
+    <Settings />
   </div>
 }
 
